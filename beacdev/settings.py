4be5ba859a194d2981bdf7c28b0204ab521d97b2
seuno <toloruntotosho@gmail>
2023-01-19 21:22:40 +0100

    Setting Allowed all hosts

--- beacdev/settings.py
+++ beacdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9wo@g(4*21-wg#uihf-(e)j6w4_gvl-jxr)9k4514cvsol9wdl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
